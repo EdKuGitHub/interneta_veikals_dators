6a60e8baff3d1dd541765669419dfe6f3865311d
EdKuGitHub <70010771+EdKuGitHub@users.noreply.github.com>
2021-01-25 14:25:59 +0200

    izmaiņas

---
+++ script.js
@@ -0,0 +1,9 @@
+function x()
+{
+    var x = 100;
+    var y = 5000;
+    var atlaide;   
+    var summa = x + y;
+    document.getElementById("rez").innerHTML = summa;
+    document.getElementById("rez").style.backgroundColor = "red";
+}
